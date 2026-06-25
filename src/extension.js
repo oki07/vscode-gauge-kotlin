27234db61e8cc631a8b5e155d00cedd420ac82c5
oki07 <ot07neoki@gmail.com>
2026-06-26 01:24:05 +0900

    feat: wire Gauge workspace activation
    Adapt parity for gauge-vscode-file-source-references-gauge-vscode-src-extension-ts-references-gauge-vscode-src-extension-ts.
    
    Reference path: references/gauge-vscode/src/extension.ts.

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,16 @@
 "use strict";
 
+const { CLI } = require("./cli");
 const { EXECUTION_COMMANDS, createGaugeExecutionController } = require("./execution/executor");
+const { GaugeClients } = require("./gaugeClients");
+const { ReferenceProvider } = require("./gaugeReference");
+const { GaugeWorkspace } = require("./gaugeWorkspace");
+const { createProjectFactory } = require("./project/projectFactory");
 const { createSpecification } = require("./specification");
+
+const MINIMUM_SUPPORTED_GAUGE_VERSION = "0.9.6";
+const PROVIDER_COMMANDS = new Set(["gauge.showReferences.atCursor"]);
+const GAUGE_WORD_PATTERN = /^(?:[*])([^*].*)$/g;
 
 const GAUGE_COMMANDS = [
   "gauge.createProject",
@@ -22,6 +31,8 @@ const GAUGE_COMMANDS = [
   "gauge.specexplorer.switchProject",
 ];
 
+let activeClientsMap;
+
 function getVscode(vscodeApi) {
   return vscodeApi || require("vscode");
 }
@@ -31,6 +42,45 @@ function notify(vscode, message) {
     return vscode.window.showInformationMessage(message);
   }
   return undefined;
+}
+
+function workspaceFolders(vscode) {
+  if (!vscode.workspace || !vscode.workspace.workspaceFolders) {
+    return [];
+  }
+  return vscode.workspace.workspaceFolders;
+}
+
+function hasActiveGaugeDocument(vscode) {
+  const editor = vscode.window && vscode.window.activeTextEditor;
+  return Boolean(editor && editor.document && editor.document.languageId === "gauge");
+}
+
+function hasGaugeProject(vscode, projectFactory) {
+  return workspaceFolders(vscode).some((folder) => projectFactory.isGaugeProject(folder.uri.fsPath));
+}
+
+function shouldStartGaugeServices(vscode, projectFactory) {
+  return hasActiveGaugeDocument(vscode) || hasGaugeProject(vscode, projectFactory);
+}
+
+function setActivatedContext(vscode) {
+  if (vscode.commands && typeof vscode.commands.executeCommand === "function") {
+    return vscode.commands.executeCommand("setContext", "gauge:activated", true);
+  }
+  return undefined;
+}
+
+function registerGaugeLanguageConfiguration(context, vscode) {
+  if (!vscode.languages || typeof vscode.languages.setLanguageConfiguration !== "function") {
+    return;
+  }
+  const disposable = vscode.languages.setLanguageConfiguration("gauge", {
+    wordPattern: GAUGE_WORD_PATTERN,
+  });
+  if (disposable) {
+    context.subscriptions.push(disposable);
+  }
 }
 
 function createCommandHandler(command, vscode, executionController, options = {}) {
@@ -57,6 +107,55 @@ function createCommandHandler(command, vscode, executionController, options = {}
   };
 }
 
+function createCli(vscode, options) {
+  const cliFactory = options.createCli || ((cliOptions) => CLI.instance(cliOptions));
+  return cliFactory({ vscode });
+}
+
+function startGaugeServices(context, vscode, options = {}) {
+  const projectFactory = options.projectFactory || createProjectFactory({
+    fileSystem: options.fileSystem,
+    pathModule: options.pathModule,
+    vscode,
+  });
+  if (!shouldStartGaugeServices(vscode, projectFactory)) {
+    return undefined;
+  }
+
+  const cli = createCli(vscode, options);
+  if (!cli) {
+    return undefined;
+  }
+  if (!cli.isGaugeInstalled() || !cli.isGaugeVersionGreaterOrEqual(MINIMUM_SUPPORTED_GAUGE_VERSION)) {
+    return notify(vscode, "Gauge is not installed or does not meet the minimum supported version.");
+  }
+
+  setActivatedContext(vscode);
+  registerGaugeLanguageConfiguration(context, vscode);
+
+  const GaugeClientsCtor = options.GaugeClients || GaugeClients;
+  const clientsMap = options.clientsMap || new GaugeClientsCtor();
+  const GaugeWorkspaceCtor = options.GaugeWorkspace || GaugeWorkspace;
+  const ReferenceProviderCtor = options.ReferenceProvider || ReferenceProvider;
+  const gaugeWorkspace = new GaugeWorkspaceCtor({
+    cli,
+    clientsMap,
+    env: options.env,
+    execSync: options.execSync,
+    fileSystem: options.fileSystem,
+    LanguageClient: options.LanguageClient,
+    pathModule: options.pathModule,
+    projectFactory,
+    RevealOutputChannelOn: options.RevealOutputChannelOn,
+    state: options.state,
+    vscode,
+  });
+  const referenceProvider = new ReferenceProviderCtor(clientsMap, { vscode });
+  activeClientsMap = clientsMap;
+  context.subscriptions.push(gaugeWorkspace, referenceProvider);
+  return gaugeWorkspace;
+}
+
 function activate(context, vscodeApi, options = {}) {
   const vscode = getVscode(vscodeApi);
   const executionController = (options.createExecutionController || createGaugeExecutionController)({
@@ -66,20 +165,29 @@ function activate(context, vscodeApi, options = {}) {
     runner: options.runner,
   });
 
-  if (vscode.commands && typeof vscode.commands.executeCommand === "function") {
-    vscode.commands.executeCommand("setContext", "gauge:activated", true);
-  }
-
-  for (const command of GAUGE_COMMANDS) {
+  for (const command of GAUGE_COMMANDS.filter((entry) => !PROVIDER_COMMANDS.has(entry))) {
     const disposable = vscode.commands.registerCommand(
       command,
       createCommandHandler(command, vscode, executionController, options),
     );
     context.subscriptions.push(disposable);
   }
+
+  startGaugeServices(context, vscode, options);
 }
 
-function deactivate() {}
+function deactivate() {
+  if (!activeClientsMap) {
+    return Promise.resolve(undefined);
+  }
+  const stopPromises = [];
+  for (const entry of activeClientsMap.values()) {
+    if (entry.client && typeof entry.client.stop === "function") {
+      stopPromises.push(entry.client.stop());
+    }
+  }
+  return Promise.all(stopPromises).then(() => undefined);
+}
 
 module.exports = {
   GAUGE_COMMANDS,
--- test/extension.test.js
+++ test/extension.test.js
@@ -1,15 +1,16 @@
 const assert = require("node:assert/strict");
 const test = require("node:test");
 
-test("activation registers every contributed Gauge command", () => {
-  const manifest = require("../package.json");
-  const extension = require("../src/extension");
+const PROVIDER_COMMANDS = new Set(["gauge.showReferences.atCursor"]);
 
+function createFakeVscode(overrides = {}) {
   const registeredCommands = [];
-  const context = { subscriptions: [] };
+  const contexts = [];
+  const languageConfigurations = [];
   const fakeVscode = {
     commands: {
-      executeCommand() {
+      executeCommand(command, key, value) {
+        contexts.push({ command, key, value });
         return undefined;
       },
       registerCommand(command, handler) {
@@ -17,20 +18,49 @@ test("activation registers every contributed Gauge command", () => {
         return { dispose() {} };
       },
     },
+    languages: {
+      setLanguageConfiguration(language, configuration) {
+        languageConfigurations.push({ language, configuration });
+        return { dispose() {} };
+      },
+    },
     window: {
+      activeTextEditor: overrides.activeTextEditor,
       showInformationMessage() {
         return undefined;
       },
     },
+    workspace: {
+      workspaceFolders: overrides.workspaceFolders,
+    },
   };
+  return {
+    contexts,
+    fakeVscode,
+    languageConfigurations,
+    registeredCommands,
+  };
+}
+
+test("activation registers core contributed Gauge commands", () => {
+  const manifest = require("../package.json");
+  const extension = require("../src/extension");
+
+  const context = { subscriptions: [] };
+  const { fakeVscode, registeredCommands } = createFakeVscode();
 
   extension.activate(context, fakeVscode);
 
   assert.deepEqual(
     registeredCommands.map((entry) => entry.command),
-    manifest.contributes.commands.map((entry) => entry.command),
+    manifest.contributes.commands
+      .map((entry) => entry.command)
+      .filter((command) => !PROVIDER_COMMANDS.has(command)),
   );
-  assert.equal(context.subscriptions.length, manifest.contributes.commands.length);
+  assert.equal(
+    context.subscriptions.length,
+    manifest.contributes.commands.length - PROVIDER_COMMANDS.size,
+  );
   assert.equal(registeredCommands.every((entry) => typeof entry.handler === "function"), true);
 });
 
@@ -38,24 +68,8 @@ test("create specification command delegates to the specification creator", () =
   const extension = require("../src/extension");
 
   let receivedOptions;
-  const registeredCommands = [];
   const context = { subscriptions: [] };
-  const fakeVscode = {
-    commands: {
-      executeCommand() {
-        return undefined;
-      },
-      registerCommand(command, handler) {
-        registeredCommands.push({ command, handler });
-        return { dispose() {} };
-      },
-    },
-    window: {
-      showInformationMessage() {
-        return undefined;
-      },
-    },
-  };
+  const { fakeVscode, registeredCommands } = createFakeVscode();
 
   extension.activate(context, fakeVscode, {
     createSpecification(options) {
@@ -77,24 +91,8 @@ test("execution commands delegate to the Gauge execution controller", () => {
   const extension = require("../src/extension");
 
   const handledCommands = [];
-  const registeredCommands = [];
   const context = { subscriptions: [] };
-  const fakeVscode = {
-    commands: {
-      executeCommand() {
-        return undefined;
-      },
-      registerCommand(command, handler) {
-        registeredCommands.push({ command, handler });
-        return { dispose() {} };
-      },
-    },
-    window: {
-      showInformationMessage() {
-        return undefined;
-      },
-    },
-  };
+  const { fakeVscode, registeredCommands } = createFakeVscode();
 
   extension.activate(context, fakeVscode, {
     createExecutionController(options) {
@@ -124,4 +122,89 @@ test("execution commands delegate to the Gauge execution controller", () => {
       args: [node],
     },
   ]);
+});
+
+test("activation starts Gauge workspace services for Gauge projects", () => {
+  const extension = require("../src/extension");
+
+  const created = {};
+  const checkedProjects = [];
+  const versions = [];
+  const context = { subscriptions: [] };
+  const { contexts, fakeVscode, languageConfigurations, registeredCommands } = createFakeVscode({
+    workspaceFolders: [{ uri: { fsPath: "/workspace/gauge" } }],
+  });
+  const cli = {
+    isGaugeInstalled() {
+      return true;
+    },
+    isGaugeVersionGreaterOrEqual(version) {
+      versions.push(version);
+      return true;
+    },
+  };
+
+  class FakeGaugeClients extends Map {
+    constructor() {
+      super();
+      created.clientsMap = this;
+    }
+  }
+
+  class FakeGaugeWorkspace {
+    constructor(options) {
+      this.options = options;
+      created.workspace = this;
+    }
+
+    dispose() {}
+  }
+
+  class FakeReferenceProvider {
+    constructor(clients, options) {
+      this.clients = clients;
+      this.options = options;
+      created.referenceProvider = this;
+    }
+
+    dispose() {}
+  }
+
+  extension.activate(context, fakeVscode, {
+    createCli(options) {
+      created.cliOptions = options;
+      return cli;
+    },
+    createExecutionController() {
+      return { handleCommand() {} };
+    },
+    GaugeClients: FakeGaugeClients,
+    GaugeWorkspace: FakeGaugeWorkspace,
+    ReferenceProvider: FakeReferenceProvider,
+    projectFactory: {
+      isGaugeProject(folder) {
+        checkedProjects.push(folder);
+        return true;
+      },
+    },
+  });
+
+  assert.deepEqual(checkedProjects, ["/workspace/gauge"]);
+  assert.equal(created.cliOptions.vscode, fakeVscode);
+  assert.deepEqual(versions, ["0.9.6"]);
+  assert.equal(created.workspace.options.cli, cli);
+  assert.equal(created.workspace.options.clientsMap, created.clientsMap);
+  assert.equal(created.workspace.options.vscode, fakeVscode);
+  assert.equal(created.referenceProvider.clients, created.clientsMap);
+  assert.equal(created.referenceProvider.options.vscode, fakeVscode);
+  assert.equal(context.subscriptions.includes(created.workspace), true);
+  assert.equal(context.subscriptions.includes(created.referenceProvider), true);
+  assert.deepEqual(contexts, [
+    { command: "setContext", key: "gauge:activated", value: true },
+  ]);
+  assert.deepEqual(
+    languageConfigurations.map((entry) => entry.language),
+    ["gauge"],
+  );
+  assert.equal(registeredCommands.some((entry) => entry.command === "gauge.showReferences.atCursor"), false);
 });
